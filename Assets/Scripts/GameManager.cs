f6aebc8440d42c562f2fc5f02a4e2a0141351fc5
000SIN <126735143+000SIN@users.noreply.github.com>
2025-03-21 17:55:19 +0900

    [Feat] Step4
    UI와 연결. refresh(), start()

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,7 +12,11 @@ public class GameManager : MonoBehaviour
 
 
     // UserData 참조 변수
-    public UserData userData;
+    public static UserData userData;
+
+    public TextMeshProUGUI userNameText;
+    public TextMeshProUGUI bankBalanceText;
+    public TextMeshProUGUI cashText;
 
     //생성자를 통해 이름과 현금, 통장 잔액 데이터 넣기
     public void Awake()
@@ -28,5 +33,21 @@ public class GameManager : MonoBehaviour
         userData = new UserData("sung", 50000, 10000);
     }
 
+    void Start()
+    {
+        Refresh();
+    }
+
+         public void Refresh()
+    {
+        // UI에 이름, 현금, 잔액을 업데이트
+        userNameText.text = userData.Username;
+        cashText.text = string.Format("{0:N0}", userData.Cash);
+        bankBalanceText.text = string.Format("{0:N0}", userData.BankBalance);
+        //string.Format을 사용하면, 텍스트를 동적으로 생성할 때 변수의 값을 다양한 형식으로 변환할 수 있음
+        //{0:N0} = 앞에 0은 첫번째 인수를 참조한다는 뜻 / N은 천단위 소수점, 0은 소수점 위에 허용가능한 숫자가 0개라는 거
+    }
 }
 
+
+  
