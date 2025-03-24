15d2a79bbd0fcec2ab6df792ab8acbb5af6ca171
000SIN <126735143+000SIN@users.noreply.github.com>
2025-03-24 15:36:20 +0900

    [Feat] step7
    입출금 창 이동

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PopupBank : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject Deposit;
+    public GameObject Withdraw;
+    public GameObject ATM; // ATM 패널 (Inspector에서 할당)
+
+    void Awake()
     {
-        
+        ATM.SetActive(true); // 게임 시작 시 ATM 패널 활성화
+
+    }
+    public void OpenDeposit()
+    {
+        Deposit.SetActive(true);
+        Withdraw.SetActive(false);
+        ATM.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OpenWithdraw()
+    { 
+        Deposit.SetActive(false);
+        Withdraw.SetActive(true);
+        ATM.SetActive(false);
     }
+
+    public void OpenATM()
+    {
+        Deposit.SetActive(false);
+        Withdraw.SetActive(false);
+        ATM.SetActive(true);
+    }
+
 }
